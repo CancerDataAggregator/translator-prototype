a8b4ba5a1558b0c22781df03fb17fb8eed1275c6
C02CR2E3ML85 <kaushik.ghose@gmail.com>
2020-12-01 13:26:42 -0500

    Rewrite translator to use namespaced column names
    1. Return is now JSON format
    2. Schema no longer needed.

--- queryt/queryt.py
+++ queryt/queryt.py
@@ -9,7 +9,17 @@ class Column:
         self.s = s
 
     def __str__(self):
-        return self.s
+        if self.parent() is not None:
+            return "_" + self.s
+        else:
+            return self.s
+
+    def parent(self):
+        _parts = self.s.split(".")
+        if len(_parts) > 1:
+            return _parts[0]
+        else:
+            return None
 
 
 class Value:
@@ -25,76 +35,23 @@ class Value:
             return str(self.s)
 
 
-class Dataset:
-    def __init__(self, client, table: str, schema: dict):
-        self.client = bigquery.Client()
-        self.table = table
-        self.unnest_dict = make_unnest_dictionary(schema)
-        self._columns = None
-
-    def columns(self):
-        # This needs to be done in a more principled manner
-        if self._columns is None:
-            _d = self.schema()
-            self._columns = [
-                _row["column_name"]
-                for _index, _row in _d.iterrows()
-                if not _row["data_type"].startswith('ARRAY')
-            ]
-            self._columns += list(self.unnest_dict.keys())
-            self._columns = sorted(self._columns)
-
-        return self._columns
-
-    def unique_values_for(self, column: Column):
-        from_clause = f"`{self.table}`"
-        if column in self.unnest_dict:
-            from_clause += f", UNNEST({self.unnest_dict.get(column)})"
-
-        sql = f"SELECT DISTINCT({column}) FROM {from_clause} ORDER BY {column} ASC"
-        return [v[column] for v in self.client.query(sql)]
-
-    def schema(self):
-        # This will be the way to get the schema going forward
-        # There is probably a better way to get the table name for this purpose
-        _parts = self.table.split('.')
-        _table_prefix = '.'.join(_parts[:-1])
-        _table_name = _parts[-1]
-
-        sql = f"SELECT * FROM `{_table_prefix}.INFORMATION_SCHEMA.COLUMNS` WHERE table_name='{_table_name}'"
-        return self.client.query(sql).to_dataframe()
-
-
-def make_unnest_dictionary(schema: dict):
-    unnest_dictionary = {}
-    for v in schema["fields"]:
-        if v["type"] != "RECORD":
-            continue
-
-        for field in v["fields"]:
-            unnest_dictionary[field["name"]] = v["name"]
-    return unnest_dictionary
-
-
 class Condition:
     def __init__(self, col: Column, operator: str, value: Value):
         self.left = col if isinstance(col, (Column, Condition)) else Column(col)
         self.operator = operator
         self.right = value if isinstance(value, (Value, Condition)) else Value(value)
 
-    def columns(self):
-        _cols = []
-        if isinstance(self.left, Column):
-            _cols += [self.left.s]
+    def _unnest_recurse(self, term: Union[Column, 'Condition']):
+        if isinstance(term, Column):
+            return [term.parent()]
         elif isinstance(self.left, Condition):
-            _cols += self.left.columns()
+            return term.columns_to_unnest()
+        else:
+            return []
 
-        if isinstance(self.right, Column):
-            _cols += [self.right.s]
-        elif isinstance(self.right, Condition):
-            _cols += self.right.columns()
-
-        return _cols
+    def columns_to_unnest(self):
+        return self._unnest_recurse(self.left) + \
+            self._unnest_recurse(self.right)
 
     def And(self, right: 'Condition'):
         return Condition(self, "AND", right)
@@ -105,15 +62,37 @@ class Condition:
     def __str__(self):
         return f"({self.left} {self.operator} {self.right})"
 
-    def sql(self, columns: List[Column], dataset: Dataset):
-        unnest = set()
-        for c in columns + self.columns():
-            if c in dataset.unnest_dict:
-                unnest.add(dataset.unnest_dict.get(c))
 
-        select = ", ".join(columns)
-        from_clause = ", ".join([dataset.table] + [f"unnest({u})" for u in list(unnest)])
-        return f"SELECT {select} FROM {from_clause} WHERE {self}"
+class Dataset:
+    def __init__(self, client, table: str):
+        self.client = bigquery.Client()
+        self.table = table
+        self._columns = None
 
-    def as_df(self, columns: List[Column], dataset: Dataset):
-        return dataset.client.query(self.sql(columns, dataset)).to_dataframe()
+    def sql(self, cond: Condition):
+        from_clause = ", ".join(
+            [self.table] +
+            [f"UNNEST({u}) AS _{u}"
+             for u in set(cond.columns_to_unnest()) if u is not None])
+        return f"SELECT * FROM {from_clause} WHERE {cond}"
+
+    def query(self, cond: Condition):
+        return self.client.query(self.sql(cond)).result()
+
+    def columns(self):
+        _parts = self.table.split(".")
+        dataset_name = _parts[:2]
+        table_name = _parts[2]
+        query = ("SELECT field_path "
+                 f"FROM `{'.'.join(dataset_name)}"
+                 ".INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`"
+                 f" WHERE table_name = '{table_name}'")
+        return [r.field_path for r in self.client.query(query)]
+
+    def unique_values_for(self, column: Column):
+        clause = f"`{self.table}`"
+        if column.parent() is not None:
+            clause += f", UNNEST({column.parent()}) as _{column.parent()}"
+
+        sql = f"SELECT DISTINCT({column}) FROM {clause} ORDER BY {column} ASC"
+        return [v.values()[0] for v in self.client.query(sql)]
